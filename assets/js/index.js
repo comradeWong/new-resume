338d370413c42754740f695a2ebcc3a47f664a2b
ComradeWong <comrade.wang.cn@gmail.com>
2022-09-07 22:25:41 +0800

    feat：继续优化移动端

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,10 @@
 function setRem() {
   // 当前页面宽度相对于 750 宽的缩放比例，可根据自己需要修改
   const cw = document.documentElement.clientWidth
-  if (cw > 480) {
-    return
-  }
-  const scale = document.documentElement.clientWidth / 750;
+  // if (cw > 480) {
+  //   return
+  // }
+  const scale = cw / 750;
   document.documentElement.style.fontSize = scale + 'px';
 }
 setRem();
